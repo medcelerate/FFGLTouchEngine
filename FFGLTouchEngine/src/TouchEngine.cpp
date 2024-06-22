7067c78ee806bb618f9a9a22896a8708a71cf259
medcelerate <evan.clark.professional@gmail.com>
2024-06-22 18:37:22 -0400

    Added int change

--- FFGLTouchEngine/src/TouchEngine.cpp
+++ FFGLTouchEngine/src/TouchEngine.cpp
@@ -619,6 +619,7 @@ void FFGLTouchEngine::ConstructBaseParameters() {
 	for (int i = MaxParamsByType + OffsetParamsByType; i < (MaxParamsByType * 2) + OffsetParamsByType; i++)
 	{
 		SetParamInfof(i, (std::string("Parameter") + std::to_string(i)).c_str(), FF_TYPE_INTEGER);
+		SetParamRange(i, 0, 1000);
 		SetParamVisibility(i, false, false);
 	}
 
